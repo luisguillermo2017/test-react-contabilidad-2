85411e9edca76fcf7d6bf98b7d93fb27784eabee
Elitaxh <vivaheredia2535@outlook.com>
2025-05-14 20:52:24 -0600

    Comoponete que con un boton para mostrar una alerta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { AlertHello } from "./components/alertHello"
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AlertHello />;
     </div>
   );
 }
